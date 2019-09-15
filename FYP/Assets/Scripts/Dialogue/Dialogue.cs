e375ee551c0f77d42a162653d0dff20e9ad3cf2b
CJW <42806340+ChanJingWen@users.noreply.github.com>
2019-09-15 11:51:12 +0800

    Dialogue script implemented

---
+++ FYP/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text textDisplay;
+    public string[] sentences; //Hold sentences
+    private int index;
+    public float typingSpeed;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
