7fc67ea8421f69660482780106e60cff30f3eda8
Zy-XD <Zy-XD@users.noreply.github.com>
2019-10-29 22:14:13 +0800

    Revert "Functionality for arcane screens added"
    This reverts commit ee9325f2d55e0868c11d931baeab01259776dc24.

--- FYP/Assets/Scripts/PlayerAbilities.cs
+++ FYP/Assets/Scripts/PlayerAbilities.cs
@@ -100,11 +100,6 @@ public class PlayerAbilities : MonoBehaviour
             o.transform.position = Vector3.Lerp(o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * throwHoldDistance, Time.deltaTime * smooth);
         }
 
-        if (!carriedObject.GetComponent<Pickupable>().carryable)
-        {
-            DropObject();
-        }
-
         o.transform.rotation = Quaternion.identity;
            
         deltaRotation = previousUp - mainCamera.transform.eulerAngles.y;
@@ -134,7 +129,7 @@ public class PlayerAbilities : MonoBehaviour
             Pickupable p = hit.collider.GetComponent<Pickupable>();
             //p.isStasis = false;
             //p.myRB.isKinematic = false;
-            if (p != null && p.carryable)
+            if (p != null)
             {
                 //Cancel stasis when the stasis coroutine is cancelled.
                 if (p.stasisCoroutine != null)
