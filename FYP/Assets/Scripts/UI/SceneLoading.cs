d7ff3b9a378e4eaf73a36f76eb9d53d225be3bf8
CJW <42806340+ChanJingWen@users.noreply.github.com>
2019-11-23 14:27:16 +0800

    Loading scene done

---
+++ FYP/Assets/Scripts/UI/SceneLoading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //Create an async operation
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+
+        while (gameLevel.progress < 1)
+        {
+            //Take the progress bar fill = async operation progress.
+            progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
