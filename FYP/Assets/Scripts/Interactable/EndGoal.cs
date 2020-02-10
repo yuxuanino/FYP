112bf25f1afee9155fb20f5d3e5da4497f09e927
Tan Lim Chai Leng <primustan@gmail.com>
2020-02-10 11:35:15 +0800

    Added EndGoal script and collider
    There is a collider at the end of the final level now which activates a placeholder end screen.
    
    I don't know if you even read these. Tell me if you do.

---
+++ FYP/Assets/Scripts/Interactable/EndGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGoal : MonoBehaviour
+{
+    public GameObject endScreen;
+    public GameObject reticleHolder;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerTPS playerScript = other.GetComponent<PlayerTPS>();
+            playerScript.canMove = false;
+            reticleHolder.SetActive(false);
+
+            endScreen.SetActive(true);
+        }
+    }
+}
