6aec0014a863c1bc50d2cd75da3394706a325e6d
Ho Yu Xuan <yuxuanino@hotmail.com>
2020-02-11 17:15:47 +0800

    Level

---
+++ FYP/Assets/Scripts/Environment/AreaTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour
+{
+    public bool playerDetected;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerDetected = true;
+            Debug.Log("Player detected = " + playerDetected);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerDetected = false;
+        }
+    }
+}
---
+++ FYP/Assets/Scripts/Environment/levelDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelDoor : MonoBehaviour
+{
+    public AreaTrigger areaTrigger;
+
+    //Target point to move towards.
+    public GameObject point1;
+    public GameObject point2;
+
+    private bool lvlCompletedd;
+
+
+    void Update()
+    {
+        //if (lvlCrystal.lvlCompleted == true)
+        //{         
+        //    transform.position = Vector3.MoveTowards(transform.position, point1.transform.position, Time.deltaTime * 3f);
+        //}
+        //if(areaTrigger.playerDetected == true)
+        //{ 
+        //    transform.position = Vector3.MoveTowards(transform.position, point2.transform.position, Time.deltaTime * 3f);
+        //}
+
+        if (areaTrigger.playerDetected == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point1.transform.position, Time.deltaTime * 3f);
+        }
+        else if (areaTrigger.playerDetected == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point2.transform.position, Time.deltaTime * 3f);
+        }
+
+    }
+
+}
