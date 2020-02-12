5ed5b6d6e0b8713e5f2417e0c868e3160af32ecb
CJW <42806340+ChanJingWen@users.noreply.github.com>
2020-02-12 14:12:30 +0800

    UI controls stuff

--- FYP/Assets/Scripts/UI/SequenceAnimator.cs
+++ FYP/Assets/Scripts/UI/SequenceAnimator.cs
@@ -1,33 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SequenceAnimator : MonoBehaviour
 {
-    public float WaitBetween;
-    public float WaitEnd;
-    
-    List<Animator> _animators;
+    //public float WaitBetween;
+    //public float WaitEnd;
+
+    //List<Animator> _animators;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        _animators = new List<Animator>(GetComponentsInChildren<Animator>());
+    //void Start()
+    //{
+    //    _animators = new List<Animator>(GetComponentsInChildren<Animator>());
 
-        StartCoroutine(DoAnimation());
-    }
+    //    StartCoroutine(DoAnimation());
+    //}
 
-    IEnumerator DoAnimation()
+    //IEnumerator DoAnimation()
+    //{
+    //    while (true)
+    //    {
+    //        foreach (var animation in _animators)
+    //        {
+    //            animation.SetTrigger("DoAnimation");
+    //            yield return new WaitForSeconds(WaitBetween);
+    //        }
+
+    //        yield return new WaitForSeconds(WaitEnd);
+    //    }
+    //}
+
+    public GameObject UIControls;
+
+    private void OnTriggerEnter(Collider other)
     {
-        while (true)
+        print("Triggered");
+        if(CompareTag("Player"))
         {
-            foreach (var animation in _animators)
-            {
-                animation.SetTrigger("DoAnimation");
-                yield return new WaitForSeconds(WaitBetween);
-            }
-
-            yield return new WaitForSeconds(WaitEnd);
+            UIControls.SetActive(false);
+            print("Player detected");
         }
     }
 }
