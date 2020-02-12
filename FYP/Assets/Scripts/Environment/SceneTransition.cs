5bddc70041d827a6cd3e8f3f06a8aeb61f6fa1c8
VVentego <53741855+VVentego@users.noreply.github.com>
2020-02-12 22:03:11 +0800

    SceneTransition Script

---
+++ FYP/Assets/Scripts/Environment/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    [Header("Create a collider as a trigger and put in scene index number you want to transition to")]
+    [Space]
+    [Header("Check build settings for scene index numbers")]
+    public int sceneToTransitionTo;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToTransitionTo);
+        }
+    }
+}
