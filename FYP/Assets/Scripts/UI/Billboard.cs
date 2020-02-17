d530639d9dfbeb570ebb9051caf5000fd11b96ea
CJW <42806340+ChanJingWen@users.noreply.github.com>
2020-02-17 12:55:24 +0800

    Billboard script

---
+++ FYP/Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform camTrans;
+
+    Quaternion originR;
+
+    private void Start()
+    {
+        originR = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = camTrans.rotation * originR;
+    }
+}
