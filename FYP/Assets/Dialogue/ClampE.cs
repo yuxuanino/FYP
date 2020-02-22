cdefe8a980e8534c58cfbeb5fbb4299fc961a0a8
CJW <42806340+ChanJingWen@users.noreply.github.com>
2020-02-22 14:19:54 +0800

    Make sure nothing broke

---
+++ FYP/Assets/Dialogue/ClampE.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampE : MonoBehaviour
+{
+    public Image emage; 
+
+    void Update()
+    {
+        Vector3 ePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        emage.transform.position = ePos;
+    }
+}
